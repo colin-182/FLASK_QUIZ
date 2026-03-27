b444da21ea10e7e2db99f95e52cd6a24022abd74
Colin Keogh <colin-182@hotmail.com>
2026-03-27 13:26:17 +0000

    typo fixed in app.py
    app.py type fixed, 1 extra _ was used when trying to test on terminal

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 
-app = Flask(__name___)
+app = Flask(__name__)
 
 app.secret_key = "change-this-to-something-secret"
 
