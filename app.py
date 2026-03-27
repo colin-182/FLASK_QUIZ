8a8a2f619e093e61f529aa83753687dcc7043b97
Colin Keogh <colin-182@hotmail.com>
2026-03-27 12:16:05 +0000

    routes 1-4 added to app.py
    added in home page, starting the quiz, showing and answering question, moving to next answer

--- app.py
+++ app.py
@@ -62,3 +62,61 @@ QUESTIONS = [
     },
 ]
 
+# Route 1: Home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Route 2: Starting the quiz
+# This should reset everything in the session so the quiz always starts fresh
+# url_for ("question") builds the URL for the "question" function below
+@app.route("/start")
+def start():
+    session["question_index"] = 0
+    session["score"] = 0
+    session["answers"] -[]
+    return redirect(url_for("question"))
+
+# Route 3: Show the current question
+@app.route("/question")
+def question():
+    index = session.get("question_index", 0)
+    if index >= len(QUESTIONS):
+        return redirect(url_for("results"))
+
+# Get just the current question dictionary from the list
+    current_question = QUESTIONS[index]
+
+# Passing data to the template using keyword arguments
+# 'number' is used to show "Question 1 of 10" etc.
+    return render_template(
+        "quiz.html",
+        question=current_question,
+        number=index + 1,
+        total=len(QUESTIONS),
+    )
+
+# Route 4: Handling the users answer
+# POST should accept form of submission, not page visits
+@app.route("/answer", methods=["POST"])
+def answer():
+    index = session.get("question_index", 0)
+    current = QUESTIONS[index]
+    chosen = request.form.get("choice")
+
+    # Check if the chosen answer matches correct answer
+    is_correct = chosen == current["answer"]
+    if is_correct:
+        session["score"] += 1
+
+    # Record this answer so we can show a review on results page
+    session["answers"] = session.get("answers", []) + [{
+        "question": current["question"],
+        "chosen": chosen,
+        "correct": current["answer"],
+        "right": is_correct,
+    }]
+
+    # Move to the next question
+    session["question_index"] = index + 1
+
